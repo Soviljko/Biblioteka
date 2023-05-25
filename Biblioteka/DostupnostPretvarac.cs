c61f846ab9d6b16d0ad722b381a8f529c5bab2a2
Soviljko <sovilj.ognjen12@gmail.com>
2023-05-25 22:13:46 +0200

    Promenjeno da pise Dostupna ili Nedostupna umesto true ili false

---
+++ Biblioteka/DostupnostPretvarac.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Biblioteka
+{
+    public class DostupnostPretvarac : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool dostupnost = (bool)value;
+            return dostupnost ? "Dostupna" : "Nedostupna";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
